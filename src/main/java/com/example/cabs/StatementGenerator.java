a960419d4b509b41a42ff7dd3a3fc03c4d9c24a5
Abhinav Manchanda <abhinav@abhinav.local>
2019-08-30 14:22:46 +0530

    Return a statement that contains the total fare for a journey.

--- src/main/java/com/example/cabs/StatementGenerator.java
+++ src/main/java/com/example/cabs/StatementGenerator.java
@@ -1,4 +1,7 @@
 package com.example.cabs;
 
 public class StatementGenerator {
+    public int create(int distance) {
+        return distance * 10;
+    }
 }
--- src/test/java/com/example/cabs/StatementGeneratorTest.java
+++ src/test/java/com/example/cabs/StatementGeneratorTest.java
@@ -2,7 +2,13 @@ package com.example.cabs;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StatementGeneratorTest {
     @Test
-    public void dummy(){}
+    public void generateStatementForAJourney(){
+        StatementGenerator generator = new StatementGenerator();
+        int totalFare = generator.create(15);
+        assertEquals(150, totalFare);
+    }
 }
