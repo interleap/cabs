54caee0a18719af44b60cf41ef1d00c627d54919
Abhinav Manchanda <abhinav@abhinav.local>
2019-09-14 16:39:21 +0530

    Extracted constants for fare per unit distance and time.

--- src/main/java/com/example/cabs/StatementGenerator.java
+++ src/main/java/com/example/cabs/StatementGenerator.java
@@ -1,7 +1,11 @@
 package com.example.cabs;
 
 public class StatementGenerator {
+
+    private static final int FARE_PER_KM = 10;
+    private static final int FARE_PER_MINUTE = 1;
+
     public int create(int distance, int timeInMinutes) {
-        return 10 * distance + timeInMinutes;
+        return FARE_PER_KM * distance + FARE_PER_MINUTE * timeInMinutes;
     }
 }
